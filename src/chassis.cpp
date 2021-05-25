1fc921b60f54ada865e49c9bedb60c9db373603e
PeterZaka <PeterZaka70@gmail.com>
2021-05-25 13:20:28 -0500

    added logger

--- src/chassis.cpp
+++ src/chassis.cpp
@@ -6,6 +6,13 @@ std::shared_ptr<ChassisController> chassis =
         .withMotors(10, -19, -20, 9)
 				// Green gearset, 2.75 in wheel diam, 8 in wheel track
         .withDimensions(AbstractMotor::gearset::green, {{2.75_in, 8_in}, imev5GreenTPR})
+        .withLogger(
+        std::make_shared<Logger>(
+            TimeUtilFactory::createDefault().getTimer(), // It needs a Timer
+            "/ser/sout", // Output to the PROS terminal
+            Logger::LogLevel::debug // Most verbose log level
+        )
+    )
         .build();
 
 std::shared_ptr<XDriveModel> drive = std::dynamic_pointer_cast<XDriveModel>(chassis->getModel());
