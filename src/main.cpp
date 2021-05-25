a748ac571a6987d379f5e78d78149c1d4858d5ad
PeterZaka <PeterZaka70@gmail.com>
2021-05-25 12:10:57 -0500

    made variables global

--- src/main.cpp
+++ src/main.cpp
@@ -29,19 +29,20 @@ void competition_initialize() {}
 void autonomous() {}
 
 
-void opcontrol() {
-	// Chassis Controller - lets us drive the robot around with open- or closed-loop control
-	std::shared_ptr<ChassisController> chassis =
-	    ChassisControllerBuilder()
-	        .withMotors(10, -19, -20, 9)
-					// Green gearset, 2.75 in wheel diam, 8 in wheel track
-	        .withDimensions(AbstractMotor::gearset::green, {{2.75_in, 8_in}, imev5GreenTPR})
-	        .build();
-					
-	auto drive = std::dynamic_pointer_cast<XDriveModel>(chassis->getModel());
+// Chassis Controller - lets us drive the robot around with open- or closed-loop control
+std::shared_ptr<ChassisController> chassis =
+    ChassisControllerBuilder()
+        .withMotors(10, -19, -20, 9)
+				// Green gearset, 2.75 in wheel diam, 8 in wheel track
+        .withDimensions(AbstractMotor::gearset::green, {{2.75_in, 8_in}, imev5GreenTPR})
+        .build();
+				
+auto drive = std::dynamic_pointer_cast<XDriveModel>(chassis->getModel());
 
-	// Master controller by default
-  Controller controller;
+// Master controller by default
+Controller controller;
+
+void opcontrol() {
 
 	while (true) {
 		// Arcade drive with the left stick
