39c7a54bde84cd1c4044cbd22a1d5b21cdd75c0b
PeterZaka <PeterZaka70@gmail.com>
2021-05-25 19:19:31 -0500

    fixed odom

--- src/chassis.cpp
+++ src/chassis.cpp
@@ -4,7 +4,10 @@
 std::shared_ptr<OdomChassisController> chassis =
     ChassisControllerBuilder()
         .withMotors(10, -19, -20, 9)
-        .withSensors(leftEncoder, rightEncoder, backEncoder)
+        .withSensors(
+            {'E', 'F', true},
+            {'G', 'H'},
+            {'A', 'B'})
         // specify the tracking wheels diameter (2.75 in), track (8 in),
         // specify the middle encoder distance (0.375 in) and diameter (2.75 in)
         .withDimensions(AbstractMotor::gearset::green, {{2.75_in, 8_in, 0.375_in, 2.75_in}, quadEncoderTPR})
