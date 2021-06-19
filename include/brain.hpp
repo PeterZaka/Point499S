79720c5f0df098bc0115e27b1988f3c354f4a333
PeterZaka <PeterZaka70@gmail.com>
2021-06-19 12:09:25 -0500

    lvgl - testing button matrix

--- include/brain.hpp
+++ include/brain.hpp
@@ -4,6 +4,4 @@
 
 void autonSelectScreenInitialize();
 
-static lv_res_t btn_rel_action(lv_obj_t* btn);
-
 void brainPrint(std::string words);
--- src/brain.cpp
+++ src/brain.cpp
@@ -1,38 +1,34 @@
 #include "brain.hpp"
 
+static const char * btnm_map[] = {"Test 1", "Test 2", "\n",
+                                  "Test 3", "Test 4", "\n", ""};
+
+lv_res_t event_handler(lv_obj_t* obj, const char* txt){
+  printf("%s was pressed\n", txt);
+  return LV_RES_OK;
+}
+
 // https://github.com/lvgl/lv_demos/tree/v5.3/lv_tutorial
 void autonSelectScreenInitialize(){
   lv_obj_t* autonScr = lv_page_create(NULL, NULL);
   lv_scr_load(autonScr);
 
-  /*Create a button*/
-  lv_obj_t* btn1 = lv_btn_create(autonScr, NULL);         /*Create a button on the currently loaded screen*/
-  lv_btn_set_action(btn1, LV_BTN_ACTION_CLICK, btn_rel_action); /*Set function to be called when the button is released*/
-
-  /*Create a label on the button (the 'label' variable can be reused)*/
-  lv_obj_t* label = lv_label_create(btn1, NULL);
-  lv_label_set_text(label, "Button 1");
-}
-
-static lv_res_t btn_rel_action(lv_obj_t* btn)
-{
-    /*Increase the button width*/
-    lv_coord_t width = lv_obj_get_width(btn);
-    lv_obj_set_width(btn, width + 20);
-
-    return LV_RES_OK;
+  lv_obj_t * btnm1 = lv_btnm_create(lv_scr_act(), NULL);
+  lv_btnm_set_map(btnm1, btnm_map);
+  lv_obj_align(btnm1, NULL, LV_ALIGN_CENTER, 0, 0);
+  lv_btnm_set_action(btnm1, event_handler);
 }
 
 void brainPrint(std::string words){
-  /*Create a Label on the currently active screen*/
-lv_obj_t* label1 =  lv_label_create(lv_scr_act(), NULL);
+    /*Create a Label on the currently active screen*/
+  lv_obj_t* label1 =  lv_label_create(lv_scr_act(), NULL);
 
-/*Modify the Label's text*/
-lv_label_set_text(label1, words.c_str());
+  /*Modify the Label's text*/
+  lv_label_set_text(label1, words.c_str());
 
-/* Align the Label to the center
- * NULL means align on parent (which is the screen now)
- * 0, 0 at the end means an x, y offset after alignment*/
-lv_obj_align(label1, NULL, LV_ALIGN_CENTER, 0, 0);
+  /* Align the Label to the center
+   * NULL means align on parent (which is the screen now)
+   * 0, 0 at the end means an x, y offset after alignment*/
+  lv_obj_align(label1, NULL, LV_ALIGN_CENTER, 0, 0);
 
 }
