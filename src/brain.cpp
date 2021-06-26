e1bddb09176b4f3130a246e74aeb263f6c4f93cb
PeterZaka <PeterZaka70@gmail.com>
2021-06-25 23:35:44 -0500

    lvgl - set size and align container

--- src/brain.cpp
+++ src/brain.cpp
@@ -30,6 +30,8 @@ static lv_res_t event_handler(lv_obj_t* obj, const char* txt){
 // https://github.com/lvgl/lv_demos/tree/v5.3/lv_tutorial
 void autonSelectScreenInitialize(){
   autonContainer = lv_obj_create(lv_scr_act(), NULL);
+  lv_obj_set_size(autonContainer, lv_obj_get_width(lv_scr_act()), lv_obj_get_height(lv_scr_act()));
+  lv_obj_align(autonContainer, NULL, LV_ALIGN_CENTER, 0, 0);
 
   lv_obj_t* btnm1 = lv_btnm_create(autonContainer, NULL);
   lv_btnm_set_map(btnm1, btnm_map);
