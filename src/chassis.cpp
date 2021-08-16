4342603cbdc00a6fba9bcd6e09149c4c519fc891
Ramen12-oss <73748774+Ramen12-oss@users.noreply.github.com>
2021-08-16 15:13:18 -0700

    Add files via upload

--- src/chassis.cpp
+++ src/chassis.cpp
@@ -4,5 +4,5 @@ Motor topLeftMotor(-3);
 Motor topRightMotor(4);
 Motor backLeftMotor(-1);
 Motor backRightMotor(2);
-MotorGroup leftSide({topLeftMotor});
-MotorGroup rightSide({topRightMotor});
+MotorGroup leftSide({topLeftMotor, backLeftMotor});
+MotorGroup rightSide({topRightMotor, backRightMotor});
